505def21d67b8a2eb6c06eb32df274c309420cde
Jonas Lopes <jmdlopes.almeida@gmail.com>
2019-10-14 22:07:14 -0300

    Projeto alerta SAMU criado

---
+++ src/main/java/com/progweb/SamuApplication.java
@@ -0,0 +1,13 @@
+package com.progweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SamuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SamuApplication.class, args);
+	}
+
+}
